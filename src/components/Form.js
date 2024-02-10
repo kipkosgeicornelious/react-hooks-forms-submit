47d1557edade3cc15d35802398ef04e0f8b289db
kipkosgeicornelious <kipkosgeicornelious6@gmail.com>
2024-02-10 21:44:17 +0300

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 
 function Form(props) {
-  const [firstName, setFirstName] = useState("Sylvia");
-  const [lastName, setLastName] = useState("Woods");
+  const [firstName, setFirstName] = useState("Cornelious");
+  const [lastName, setLastName] = useState("Kipkosgei");
+  const [submittedData, setSubmittedData] = useState([])
+  const [errors, setErrors] = useState([])
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +14,52 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(e){
+    e.preventDefault();
+   
+    if(firstName.length > 0){
+      const formData = {
+        firstName: firstName,
+        lastName: lastName,
+      };
+      const dataArray = [...submittedData, formData]
+      setSubmittedData(dataArray)
+      setFirstName("")
+      setLastName("")
+      setErrors([])
+    } else{
+      setErrors(["Name is required!"])
+    }
+
+
+    
+  }
+
+  const listOfSubmissions = submittedData.map((data, index)=>{
+    return(
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    )
+  });
+
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text"   id="firstNameInput" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text"    id="lastNameInput"onChange={handleLastNameChange} value={lastName} />
+        <button type="submit" onClick={()=> alert("Submitted successfully")}>Submit</button>
+      </form>
+      {errors.length > 0
+      ? errors.map((error, index)=>(
+        <p key={index} style={{color: "red"}}>{error}</p>
+      ))
+      : null}
+      <h3>Submissions</h3>
+      {listOfSubmissions}
+    </div>
+    
+    
   );
 }
 
